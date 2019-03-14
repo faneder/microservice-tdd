dd5d96d5110cd910f573e0b035e3662074e05232
Eder Fan <fanedercode@gmail.com>
2019-03-14 09:31:36 +0800

    follow PEP8 coding style

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -11,6 +11,7 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
+
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
